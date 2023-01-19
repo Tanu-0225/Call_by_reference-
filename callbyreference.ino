ae07e52b887cd3b63b4590791fcbf20aa1543652
Tanu-0225 <tanusundesha11@gmail.com>
2023-01-19 15:09:48 +0530

    call by reference

---
+++ callbyreference.ino
@@ -0,0 +1,22 @@
+int inta = 123, intb = 456;
+void swap(int *a, int *b)
+{  
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+
+  
+}
+void setup() {
+  Serial.begin(9600);// put your setup code here, to run once:
+
+}
+
+void loop() {
+  swap(&inta , &intb);// put your main code here, to run repeatedly:
+
+  Serial.print(inta);
+  Serial.print(" ");
+  Serial.println(intb);
+
+}
